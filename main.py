2a0af2cdcce7b81ed7718ab8aeec98f1b3fa7bf7
Jared Riley <2JRemail@gmail.com>
2022-10-13 21:49:33 -0500

    Tried to get corralation to work, sort of works but need to fix still

--- main.py
+++ main.py
@@ -8,12 +8,27 @@ import matplotlib
 # matplotlib.use('TkAgg', force=True)
 import matplotlib.pyplot as plt
 samplerate, data = wavfile.read('gong.wav')
-data1=data
-corr = np.correlate(data, data1, mode='valid')
-Time = np.linspace(0, samplerate, len(corr))
+
+#autocorrelation of the .wav file
+data_a = np.float32(data)
+corr = signal.correlate(data_a, data_a)
+lags = signal.correlation_lags(len(data_a), len(data_a))
+corr = corr / np.max(corr)
+lag = lags[np.argmax(corr)]
+################
+
+#Corralate take 2 - This sort of works but need to dig into more.
+r = np.correlate(data, data, mode='full')[len(data)-1:]
+
+
+
+
+# data1=data
+# corr = np.correlate(data, data1, mode='valid')
+# Time = np.linspace(0, samplerate, len(corr))
 # playsound('gong.wav')
 #Observation, the sound went gong.
 plt.figure(figsize=(15,8))
 # plt.plot(data)
-plt.plot(corr,Time)
+plt.plot(r[:1000])
 plt.show()
