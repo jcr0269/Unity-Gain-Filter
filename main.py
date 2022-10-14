6c74e714b86e100fd1cb9e040b44cbe1411e913b
Jared <2JRemail@gmail.com>
2022-10-13 19:16:12 -0500

    Trying to get auto correlate to work correctly

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-yes
+import scipy.signal
+from scipy.io import wavfile
+from playsound import playsound
+import soundfile as sf
+import scipy.signal as signal
+import numpy as np
+import matplotlib
+# matplotlib.use('TkAgg', force=True)
+import matplotlib.pyplot as plt
+samplerate, data = wavfile.read('gong.wav')
+data1=data
+corr = np.correlate(data, data1, mode='valid')
+Time = np.linspace(0, samplerate, len(corr))
+# playsound('gong.wav')
+#Observation, the sound went gong.
+plt.figure(figsize=(15,8))
+# plt.plot(data)
+plt.plot(corr,Time)
+plt.show()
