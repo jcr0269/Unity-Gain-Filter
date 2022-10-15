2be688418fb0643b23fa98098199cb1cae068e0d
Jared Riley <2JRemail@gmail.com>
2022-10-15 12:39:28 -0500

    Currently I have the auto correlation working and the original wav data being plotted and now normalized

--- main.py
+++ main.py
@@ -19,21 +19,25 @@ lag = lags[np.argmax(corr)]
 
 #Corralate take 2 - This sort of works but need to dig into more.
 # r = np.correlate(data, data, mode='full')[len(data)-1:]
+power = data/max(data)
 
 #Ran for auto correlation
-r = scipy.signal.correlate(data, data, 'full')
+r = scipy.signal.correlate(data, power, 'full')
 
 # data1=data
 # corr = np.correlate(data, data1, mode='valid')
 # Time = np.linspace(0, samplerate, len(corr))
 # playsound('gong.wav')
 
-#Observation, the sound went gong.
+#Observation, the sound went gong.``
 plt.figure(figsize=(15,8))
 #data is for the origional sound wave
 # plt.plot(data)
+
+
+
 #x limit so that the wave form isnt to big on the plot
 plt.xlim([40000,44000])
-# plt.xlim([20000,21500])
+
 plt.plot(r)
 plt.show()
