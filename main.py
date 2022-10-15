77faf03adefcb81df916a5c26ea981e49d1c7f79
Jared Riley <2JRemail@gmail.com>
2022-10-15 13:13:02 -0500

    just finished adding the sine wave and added it to the data then did auto correlation on that corrupted signal.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib
 # matplotlib.use('TkAgg', force=True)
 
 import matplotlib.pyplot as plt
-samplerate, data = wavfile.read('gong.wav')
+samplerate, data = wavfile.read('chirp.wav')
 
 #Normalized signal
 power = data/data.max()
@@ -16,13 +16,27 @@ power = data/data.max()
 #Ran for auto correlation
 r = scipy.signal.correlate(power, power, 'full')
 
+#making a sine wave and adding it to data
+data_a = np.array(data)
+length = len(data)
+t = np.linspace(0, 1000, num=length)
+the_wave = .5*np.sin(2*np.pi*60*t)
+new_data = the_wave + data_a
+
+new_data_norm = new_data/new_data.max()
+r_new_data = scipy.signal.correlate(new_data_norm,new_data_norm,'full')
 # playsound('gong.wav')
 #Observation, the sound went gong.``
-plt.figure(figsize=(15,8))
+
 #data is for the origional sound wave
 plt.plot(data)
 plt.show()
 plt.plot(r)
 plt.show()
+#psd is welch
 plt.psd(power)
+plt.show()
+plt.plot(new_data)
+plt.show()
+plt.plot(r_new_data)
 plt.show()
